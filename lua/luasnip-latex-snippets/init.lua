57e4330f967da6ac64bf9b05b06ecb3c4a120ec3
NotRexButCaesar <68972888+NotRexButCaesar@users.noreply.github.com>
2025-10-15 04:41:17 -0700

    feat!(wA): remove full stops in block math snippet (`dm`) (#49)
    - Also, remove erroneous slash/divide in math_i.lua (`ddx`, `d/dx`)

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -124,7 +124,7 @@ M.setup_markdown = function()
   -- tex delimiters
   local normal_wA_tex = {
     parse_snippet({ trig = "mk", name = "Math" }, "$${1:${TM_SELECTED_TEXT}}$"),
-    parse_snippet({ trig = "dm", name = "Block Math" }, "$$\n\t${1:${TM_SELECTED_TEXT}}\n.$$"),
+    parse_snippet({ trig = "dm", name = "Block Math" }, "$$\n\t${1:${TM_SELECTED_TEXT}}\n$$"),
   }
   vim.list_extend(filtered, normal_wA_tex)
 
